d50f61265e947b7947928da3235558c5479e7e9f
super-xb <1229162048@qq.com>
2023-12-11 00:57:05 +0800

    去掉了SSL支持

--- pakistan_news/settings.py
+++ pakistan_news/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Pakistan.apps.PakistanConfig',
-    "sslserver",
 ]
 
 MIDDLEWARE = [
@@ -117,5 +116,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-SECURE_SSL_REDIRECT = True
