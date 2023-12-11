a52af8ad576acf23aeba3ebbf889bc7ccec0236a
0d0-0 <liuyifan115@hotmail.com>
2023-12-11 21:34:45 +0800

    更新数据库

--- Pakistan/apps.py
+++ Pakistan/apps.py
@@ -1,9 +1,9 @@
 from django.apps import AppConfig
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
+# from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin
+# from django.contrib.auth.models import User
 
-admin.site.register(User, UserAdmin)
+# admin.site.register(User, UserAdmin)
 
 class PakistanConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
