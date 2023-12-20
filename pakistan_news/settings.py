b9add59b0d0375cbb8126033df69e2a7e9ab8a2d
yd <2648525276@qq.com>
2023-12-20 16:02:15 +0800

    后台修改

--- pakistan_news/settings.py
+++ pakistan_news/settings.py
@@ -117,3 +117,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SIMPLEUI_LOGO = 'Pakistan\static\img\超级Django汪汪队.png'
